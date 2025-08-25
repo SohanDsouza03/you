1e67c08b8c1c08eb09fa3e7ce994987249a1415f
sohan dsouza <sohandsouza03@gmail.com>
2025-08-25 11:57:06 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js code has newer version
+
+// alert(3+3) //we are using node js not browser
+
+
+console.log(3+3);
+
+console.log("Carol Dsouza");
+
+let name = "sohan"
+let age = 21
+let isLoggedin = false
+
+
+//number => 2 to power 53
+//bignit
+//string =>""
+//boolean =>true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof "carol Dsouza");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+
+
+
+
+

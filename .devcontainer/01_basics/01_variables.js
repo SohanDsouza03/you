19953e7f20c6a9bbefb6f6ee8cf337d5c2b4f0b9
sohan dsouza <sohandsouza03@gmail.com>
2025-08-25 11:57:22 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_id = 9113
+let account_email = "sohan03@gmail.com"
+var accpassword = "12345"
+account_city = "honnavar"
+let account_sd;
+
+// account_id = 2
+
+// console.log(account_id);
+
+account_email="hchc@gmail.com"
+accpassword = "4733333"
+account_city="bangalore"
+
+console.table([account_id,account_email,accpassword,account_city])
+

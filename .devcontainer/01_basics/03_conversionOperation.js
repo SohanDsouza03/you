848753a3236d260a1ac80a525b2d67f571eb1cc1
sohan dsouza <sohandsouza03@gmail.com>
2025-08-27 07:16:50 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "sohan"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let ValueInNumber= Number(score)
+//console.log(typeof ValueInNumber);
+//console.log( ValueInNumber);
+
+//"33" => 33
+//"33abc" => NAN
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);

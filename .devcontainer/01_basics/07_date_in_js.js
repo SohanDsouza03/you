aeb6f2ecfd77b7de1ed3d2e70b762f3656d19a6d
sohan dsouza <sohandsouza03@gmail.com>
2025-09-01 18:33:41 +0000

    date

---
+++ .devcontainer/01_basics/07_date_in_js.js
@@ -0,0 +1,31 @@
+//date
+
+let mydate=new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+
+console.log(typeof mydate);
+
+let myCreatedData = new Date(2023,2,23)
+console.log(myCreatedData.toDateString());
+
+let myCreatedData1 = new Date(2023,2,23,5,3)
+console.log(myCreatedData1.toLocaleString());
+
+let newdate= new Date()
+console.log(newdate);
+console.log(newdate.getMonth());
+// console.log(newdate.getday());
+
+
+
+
+
+
+
+
+
+
+

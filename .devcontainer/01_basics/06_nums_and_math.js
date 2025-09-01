e1d8bf9a9ce959d810b30babb415148f34190e61
sohan dsouza <sohandsouza03@gmail.com>
2025-09-01 13:47:42 +0000

    math and number

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const OtherNumber = 123.8966
+
+// console.log(OtherNumber.toPrecision(3));
+
+const hundreds = 100000
+
+// console.log(hundreds.toLocaleString('en-In'));
+
+// +++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,1,6,4,2,7,3,8,3,9));
+// console.log(Math.max(4,6,7,2,9,2,6,2,8,9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1 );
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
+
+
+
+
+
+
+
+
+
+

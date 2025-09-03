c2f36256bd8440a9bc7acaa3819f03a3878b6096
sohan dsouza <sohandsouza03@gmail.com>
2025-09-03 16:06:10 +0000

    array

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,62 @@
+const myarr = [0,1,2,3,4,5]
+const hero = ["sohan","aaron","david"]
+const myarr2 = [1,2,3,4,5,6]
+console.log(myarr[4]);
+
+// myarr.push(1)
+// console.log(myarr);
+
+// myarr.pop()
+// console.log(myarr);
+
+// myarr.push(9)
+// console.log(myarr);
+
+// myarr.unshift(9)
+// console.log(myarr);
+
+// myarr.shift()
+// console.log(myarr);
+
+
+myarr.shift()
+console.log((myarr));
+
+
+console.log(myarr.includes(5));
+
+console.log(myarr.indexOf(3));
+
+const newarr = myarr.join()
+
+console.log(myarr);
+console.log( newarr);
+
+// slice,splice
+
+// console.log("A ",myarr);
+// const myNew1 =myarr.slice(1,3)
+// console.log(myNew1);
+// console.log("B ",myarr);
+
+const myNew2 = myarr.slice(2,5)         //using slice
+console.log(myNew2);
+
+
+const myNew3 = myarr.splice(2,5)         //using splice
+console.log(myNew3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

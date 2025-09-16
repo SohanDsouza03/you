3e6cb312f9b76039fe87489c7c99c3c58968eb2c
SohanDsouza03 <sohandsouza03@gmail.com>
2025-09-17 00:36:29 +0530

    getter setter

---
+++ 10_classes_And_oop/getter_setter.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const sohan = new User("s@sohan.ai", "abc")
+console.log(sohan.email);
